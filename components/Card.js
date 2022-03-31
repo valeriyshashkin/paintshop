fef68801b2940592ea59bf80839815a1f1d6175f
valeriyshashkin <velianjanjan@gmail.com>
2022-03-31 20:08:29 +0500

    адаптивный вид для карточек товара и страницы товара

--- components/Card.js
+++ components/Card.js
@@ -18,17 +18,21 @@ export default function Card({ title, price, cart, href }) {
           <p className="price">{price} ₽</p>
           <div className="button">
             {cart ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="white"
-              >
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M1.41 1.13L0 2.54l4.39 4.39 2.21 4.66-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h7.46l1.38 1.38c-.5.36-.83.95-.83 1.62 0 1.1.89 2 1.99 2 .67 0 1.26-.33 1.62-.84L21.46 24l1.41-1.41L1.41 1.13zM7 15l1.1-2h2.36l2 2H7zM20 4H7.12l2 2h9.19l-2.76 5h-1.44l1.94 1.94c.54-.14.99-.49 1.25-.97l3.58-6.49C21.25 4.82 20.76 4 20 4zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2z" />
-              </svg>
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  width="24px"
+                  fill="white"
+                >
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M1.41 1.13L0 2.54l4.39 4.39 2.21 4.66-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h7.46l1.38 1.38c-.5.36-.83.95-.83 1.62 0 1.1.89 2 1.99 2 .67 0 1.26-.33 1.62-.84L21.46 24l1.41-1.41L1.41 1.13zM7 15l1.1-2h2.36l2 2H7zM20 4H7.12l2 2h9.19l-2.76 5h-1.44l1.94 1.94c.54-.14.99-.49 1.25-.97l3.58-6.49C21.25 4.82 20.76 4 20 4zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2z" />
+                </svg>
+                <div className="buy-text">Убрать</div>
+              </>
             ) : (
+              <>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="24px"
@@ -39,6 +43,8 @@ export default function Card({ title, price, cart, href }) {
                 <path d="M0 0h24v24H0V0z" fill="none" />
                 <path d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
               </svg>
+              <div className="buy-text">В корзину</div>
+              </>
             )}
           </div>
         </div>
@@ -100,6 +106,18 @@ export default function Card({ title, price, cart, href }) {
           }
         }
 
+        .buy-text {
+          display: none;
+          padding-left: 8px;
+          color: white;
+        }
+
+        @media (min-width: 540px) {
+          .buy-text {
+            display: block;
+          }
+        }
+
         .description {
           padding-left: 10px;
           display: flex;
@@ -119,6 +137,18 @@ export default function Card({ title, price, cart, href }) {
           display: inline-flex;
           padding: 8px;
           border-radius: var(--radius);
+          align-items: center;
+        }
+
+        @media (min-width: 540px) {
+          .price-and-button {
+            flex-direction: column;
+            align-items: flex-start;
+          }
+
+          .button {
+            margin-top: 16px;
+          }
         }
       `}</style>
     </div>
--- pages/product/[id]/[slug].js
+++ pages/product/[id]/[slug].js
@@ -8,21 +8,42 @@ export default function Product() {
   const { id, slug } = router.query;
 
   return (
-    <>
-      <div className="image"></div>
-      <h1>Белая краска тратата тратата тратата</h1>
-      <p className="price">1000 ₽</p>
-      <div className="button">Добавить в корзину</div>
-      <p className="description-title">Описание</p>
-      <p className="description">
-        Здесь должно быть описание тратата тратата тратата тратата тратата
-        тратата тратата тратата тратата тратата тратата тратата тратата
-      </p>
+    <div className="page">
+      <div className="image half"></div>
+      <div className="after-image half">
+        <h1>Белая краска тратата тратата тратата</h1>
+        <p className="price">1000 ₽</p>
+        <div className="button">Добавить в корзину</div>
+        <p className="description-title">Описание</p>
+        <p className="description">
+          Здесь должно быть описание тратата тратата тратата тратата тратата
+          тратата тратата тратата тратата тратата тратата тратата тратата
+        </p>
+      </div>
       <style jsx>{`
         .image {
           width: 100%;
           padding-bottom: 100%;
           background: gray;
+        }
+
+        @media (min-width: 700px) {
+          .image {
+            padding-bottom: 50%;
+          }
+
+          .after-image {
+            margin-left: 10px;
+          }
+
+          .half {
+            width: 50%;
+          }
+
+          .page {
+            display: flex;
+            align-items: flex-start;
+          }
         }
 
         h1 {
@@ -56,7 +77,7 @@ export default function Product() {
           margin-top: 0;
         }
       `}</style>
-    </>
+    </div>
   );
 }
 
