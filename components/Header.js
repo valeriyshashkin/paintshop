bac3c96f9518154cace4331f7116335049f30be7
valeriyshashkin <velianjanjan@gmail.com>
2022-06-09 20:14:14 +0500

    добавил счетчик товаров в корзине

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,16 @@ import { ShoppingBagIcon } from "@heroicons/react/outline";
 import { UserCircleIcon } from "@heroicons/react/outline";
 import { XCircleIcon } from "@heroicons/react/outline";
 import { useState } from "react";
+import fetcher from "../utils/fetcher";
+import { getCookie } from "cookies-next";
+import useSWR from "swr";
 
 export default function Header({ preview }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
+
+  const { data } = useSWR(getCookie("cart") ? "/api/cart" : null, fetcher);
 
   function handleEmail(e) {
     setEmail(e.target.value);
@@ -96,11 +101,25 @@ export default function Header({ preview }) {
             </button>
           </label>
         </label>
-        <Link href="/cart">
-          <a>
-            <ShoppingBagIcon className="w-6 h-6" />
-          </a>
-        </Link>
+        {data && data.length ? (
+          <div className="indicator">
+            <span className="indicator-item badge badge-sm badge-primary">
+              {data.length}
+            </span>
+
+            <Link href="/cart">
+              <a>
+                <ShoppingBagIcon className="w-6 h-6" />
+              </a>
+            </Link>
+          </div>
+        ) : (
+          <Link href="/cart">
+            <a>
+              <ShoppingBagIcon className="w-6 h-6" />
+            </a>
+          </Link>
+        )}
       </header>
     </>
   );
