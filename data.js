828d4139a58675a43d5ac999498025805a336c63
valeriyshashkin <velianjanjan@gmail.com>
2022-09-12 21:45:47 +0500

    перехожу на локальные файлы js вместо yaml

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = {
+  email: "example@example.com",
+  products: [
+    {
+      name: "Масляные краски",
+      description: "Очень длинное описание для красок",
+      price: 90,
+      image: "/1.jpg",
+    },
+    {
+      name: "Баллончик с краской",
+      description: "Просто баллончик с краской",
+      price: 2000,
+      image: "/2.jpeg",
+    },
+    {
+      name: "Очень-очень много розовых банок с краской",
+      description: "Много",
+      price: 4000,
+      image: "/3.webp",
+    },
+    {
+      name: "Банки с краской вид сверху",
+      description: "Сверху всегда лучше",
+      price: 5000,
+      image: "/4.jpg",
+    },
+    {
+      name: "Использованные банки с краской",
+      description: "Почему бы и нет?",
+      price: 100,
+      image: "/5.jpg",
+    },
+    {
+      name: "Стенка из банок с краской",
+      description: "Много-много краски!",
+      price: 6000,
+      image: "/6.jpg",
+    },
+  ],
+};
+
+export default data;
