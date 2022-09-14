a02bd9f247be619b9604daa9ec4e7ed62d4b8b4d
valeriyshashkin <velianjanjan@gmail.com>
2022-09-14 23:29:20 +0500

    сделал добавление удаление товаров в корзине, но почему то изменение в jotai не показывается сразу

--- components/Card.js
+++ components/Card.js
@@ -15,56 +15,75 @@ export default function Card({ name, price, image, forCart }) {
     setMount(true);
   }, []);
 
-  function minus(e) {
-    e.stopPropagation();
+  function minus() {
+    const prevAmount = cart.find(
+      (c) => c.name === slugify(name).toLowerCase()
+    ).amount;
+
+    if (prevAmount === 1) {
+      setCart(cart.filter((c) => c.name !== slugify(name).toLowerCase()));
+      return;
+    }
+
+    const updatedCart = cart.map((c) =>
+      c.name === slugify(name).toLowerCase()
+        ? { name: c.name, amount: c.amount - 1 }
+        : c
+    );
+
+    setCart(updatedCart);
   }
 
-  function plus(e) {
-    e.stopPropagation();
-    console.log("ffsfsd");
+  function plus() {
+    const updatedCart = cart.map((c) =>
+      c.name === slugify(name).toLowerCase()
+        ? { name: c.name, amount: c.amount + 1 }
+        : c
+    );
+
+    setCart(updatedCart);
   }
 
   return (
-    <Link href={`/product/${slugify(name).toLowerCase()}`}>
-      <a className="block">
-        <div className="py-2 flex items-center">
-          <div className="max-w-[100px] md:max-w-[200px] w-full">
-            <div className="w-full pb-full relative block">
-              <Image src={image} layout="fill" objectFit="cover" alt="" />
-            </div>
-          </div>
-          <div className="px-5">
-            <h5 className="text-2xl pb-2 font-semibold tracking-tight">
-              {name}
-            </h5>
-            <div className="flex justify-between items-center">
-              <span className="text-xl md:text-3xl font-bold">{price} ₽</span>
-            </div>
-          </div>
-          {forCart && mount && (
-            <div className="flex items-center ml-auto">
-              <button
-                onClick={minus}
-                className="border-blue-500 border text-blue-500 w-10 h-10 text-lg rounded-xl"
-              >
-                -
-              </button>
-              <div className="px-4 font-bold text-lg">
-                {
-                  cart.find((c) => c.name === slugify(name).toLowerCase())
-                    .amount
-                }
+    <div className="relative">
+      <Link href={`/product/${slugify(name).toLowerCase()}`}>
+        <a className="block">
+          <div className="py-2 flex items-center">
+            <div className="max-w-[100px] md:max-w-[200px] w-full">
+              <div className="w-full pb-full relative block">
+                <Image src={image} layout="fill" objectFit="cover" alt="" />
               </div>
-              <button
-                onClick={plus}
-                className="border-blue-500 border text-blue-500 w-10 h-10 text-lg rounded-xl"
-              >
-                +
-              </button>
             </div>
-          )}
+            <div className="px-5">
+              <h5 className="text-2xl pb-2 font-semibold tracking-tight">
+                {name}
+              </h5>
+              <div className="flex justify-between items-center">
+                <span className="text-xl md:text-3xl font-bold">{price} ₽</span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </Link>
+      {forCart && mount && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 flex items-center ml-auto">
+          <button
+            onClick={minus}
+            className="border-blue-500 border text-blue-500 w-10 h-10 text-lg rounded-xl"
+          >
+            -
+          </button>
+          <div className="px-4 font-bold text-lg">
+            {cart.find((c) => c.name === slugify(name).toLowerCase()).amount}
+          </div>
+          <button
+            onClick={plus}
+            className="border-blue-500 border text-blue-500 w-10 h-10 text-lg rounded-xl"
+          >
+            +
+          </button>
         </div>
-      </a>
-    </Link>
+      )}
+    </div>
   );
 }
